a55899a7e634f74f933da3e84952ec984424cfde
rms21 <rasoul_salehi@ymail.com>
2018-09-28 18:21:14 +0330

    add routes for auth modules and create files

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+// auth modules
+import Login from './modules/auth/Login.jsx';
+import Register from './modules/auth/Register.jsx';
+
 class Router extends Component {
     render() {
       return (
         <BrowserRouter>
           <Switch>
-
+            <Route path='/login' component={Login} exact />
+            <Route paht='/register' component={Register} exact />
           </Switch>
         </BrowserRouter>
       );

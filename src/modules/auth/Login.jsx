99734fa00c14a965ebd91cc8fd4d76d246707a9e
rms21 <rasoul_salehi@ymail.com>
2018-10-04 18:44:24 +0330

    move auth styles to seperate files

--- src/modules/auth/Login.jsx
+++ src/modules/auth/Login.jsx
@@ -3,7 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { AccountCircle } from '@material-ui/icons';
 import { Button, Avatar, Input, FormControl, InputAdornment, Grid } from '@material-ui/core';
-import BackgorundImage from '../../assets/images/auth/bg.jpg';
+import styles from './styles/Login.js';
 
 
 
@@ -60,66 +60,5 @@ class Login extends Component {
 		);
 	}
 }
-
-const styles = theme => ({
-	container: {
-		height: '100vh',
-	},
-	formContainer: {
-		textAlign: 'center'
-	},
-	logo: {
-		height: '128px',
-		width: '128px',
-		marginRight: 'auto',
-		marginLeft: 'auto'
-	},
-	usernameFormControl: {
-		marginTop: '30px',
-		marginBottom: '30px'
-	},
-	input: {
-		paddingRight: '10px !important',
-		color: 'white'
-	},
-	inputUnderline: {
-		borderBottom: '1px solid white',
-	},
-	userIcon: {
-		marginRight: 0,
-		color: 'white'
-	},
-	loginButton: {
-		marginBottom: '20px'
-	},
-	registerLink: {
-		textDecoration: 'none',
-		color: 'white'
-	},
-	backgroundImage: {
-		zIndex: '-1',
-		position: 'fixed',
-		height: '100%',
-		width: '100%',
-		top: 0,
-		left: 0,
-		background: `url(${BackgorundImage})`,
-		backgroundPosition: 'center',
-		backgroundRepeat: 'no-repeat',
-		backgroundSize: 'cover',
-		'&::after': {
-			zIndex: 1,
-			position: 'absolute',
-			width: '100%',
-			height: '100%',
-			left: 0,
-			top: 0,
-			display: 'block',
-			backgroundColor: 'rgba(0,0,0, 0.7)',
-			content: '""'
-		}
-	}
-});
-
 
 export default withStyles(styles)(Login);
--- src/modules/auth/Register.jsx
+++ src/modules/auth/Register.jsx
@@ -10,7 +10,7 @@ import compose from 'recompose/compose';
 import { withStyles } from '@material-ui/core/styles'
 import { Grid, FormControl, Input, InputAdornment, Avatar, Button } from '@material-ui/core';
 import { Phone, LocationOn, StoreMallDirectory, Description } from '@material-ui/icons';
-import BackgorundImage from '../../assets/images/auth/bg.jpg';
+import styles from './styles/Register.js';
 
 
 // config map
@@ -253,87 +253,6 @@ class Register extends Component {
 	}
 }
 
-const styles = theme => ({
-	container: {
-		height: '100vh',
-	},
-	logo: {
-		height: '128px',
-		width: '128px',
-		marginRight: 'auto',
-		marginLeft: 'auto',
-		marginTop: '50px',
-		marginBottom: '30px'
-	},
-	inputGridContainer: {
-		marginBottom: '20px'
-	},
-	input: {
-		paddingRight: '10px !important',
-		color: 'white'
-	},
-	inputUnderline: {
-		borderBottom: '1px solid white',
-	},
-	inputIcon: {
-		marginRight: 0,
-		color: 'white'
-	},
-	uploadMessageContainer: {
-		width: '100%',
-		height: '100%',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	textWhite: {
-		color: 'white'
-	},
-	label: {
-		color: 'white',
-		width: '100%',
-		textAlign: 'right'
-	},
-	imagesImage: {
-		width: 'calc(25% - 10px)',
-		height: '150px',
-		margin: '5px'
-	},
-	loginLink: {
-		textDecoration: 'none',
-		color: 'white',
-		marginBottom: '50px'
-	},
-	backgroundImage: {
-		zIndex: '-1',
-		position: 'fixed',
-		height: '100%',
-		width: '100%',
-		top: 0,
-		left: 0,
-		background: `url(${BackgorundImage})`,
-		backgroundPosition: 'center',
-		backgroundRepeat: 'no-repeat',
-		backgroundSize: 'cover',
-		'&::after': {
-			zIndex: 1,
-			position: 'absolute',
-			width: '100%',
-			height: '100%',
-			left: 0,
-			top: 0,
-			display: 'block',
-			backgroundColor: 'rgba(0,0,0, 0.7)',
-			content: '""'
-		}
-	},
-	"@global": {
-		'.mapboxgl-canvas': {
-			position: 'relative !important'
-		}
-	}
-});
 
 const mapStateToProps = ({ authRegister }) => {
 	const { form } = authRegister;

5d0080565689b09e66306d45098b9bb6ba52a31a
rms21 <rasoul_salehi@ymail.com>
2018-10-08 12:50:50 +0330

    remove console log my plugins actions

--- src/redux/actions/plugins/MyPluginsActions.js
+++ src/redux/actions/plugins/MyPluginsActions.js
@@ -1,7 +1,6 @@
 import { PLUGINS_MY_PLUGINS_TOGGLE_PLUGIN_DIALOG } from '../../types';
 
 export const pluginsMyPluginsTogglePluginDialog = () => {
-  console.log('fuck');
   return {
     type: PLUGINS_MY_PLUGINS_TOGGLE_PLUGIN_DIALOG
   }
